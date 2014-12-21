4cba50ba2d0fa4c439f6ef97b5343f3d29271eac
psy_ipm <psy.ipm@gmail.com>
2014-12-21 20:20:59 +0200

    Initial commit. New Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Recipe4you::Application.config.secret_key_base = '0ead8e5f05b0aa91d00e177a48f8e2d5d3c3f31e45c86177123e62e5c214c21d7bf103a86397824571080f47c8bae0428ce6d2e8b26c1a782dc4b3724efb8c4f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Recipe4you::Application.config.session_store :cookie_store, key: '_recipe4you_session'
